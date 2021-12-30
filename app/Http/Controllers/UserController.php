cd5c87b84f2ffd9f134fcaa784c1c7f562ddbcb1
otikojairus <otikojairus@gmail.com>
2021-12-30 11:24:49 +0300

    loggin tweak

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -36,7 +37,7 @@ class UserController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store2(Request $request)
     {
         // dd($request);
         $rules = [
@@ -62,13 +63,44 @@ class UserController extends Controller
        // return response()->json(["woork"=>"yee"]);
     }
 
+
+    public function store(Request $request){
+        // dd($request);
+        $rules = [
+            'emailId' => 'unique:accounts|required',
+            'AccountType' => 'required',
+            'password' => 'required',
+        ];
+
+        $input     = $request->only('emailId', 'AccountType','password');
+        $validator = Validator::make($input, $rules);
+
+        if ($validator->fails()) {
+            return response()->json(['success' => false, 'error' => $validator->messages()]);
+        }
+
+        $user = new Account();
+        $user->emailId = $request->emailId;
+        $user->accountType = $request->AccountType;
+        $user->password = $request->password;
+
+       if( $user->save() ){
+           return response()->json(['success' => true, 'message' => 'user has registered successfully.', "data"=>$user]);
+       }else{
+           return response()->json(['success' => false, 'error' => 'we encountered an error']);
+       }
+
+
+
+    }
+
     /**
      * Display the specified resource.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function login(){
+    public function login1(){
         if(Auth::attempt(['emailId' => request('emailId'), 'password' => request('password')])){
             $user = Auth::user();
             return response()->json(['success'=>true,'userDetails'=>$user ], 200);
@@ -76,6 +108,32 @@ class UserController extends Controller
         else{
             return response()->json(['success'=>false,'error'=>'wrong login credentials' ], 401);
         }
+    }
+
+    public function login(Request $request){
+        $rules = [
+            'emailId' => 'required',
+            'password' => 'required',
+        ];
+
+        $input     = $request->only('emailId','password');
+        $validator = Validator::make($input, $rules);
+
+        if ($validator->fails()) {
+            return response()->json(['success' => false, 'error' => $validator->messages()]);
+        }
+
+        $emailId = $request->emailId;
+        $password = $request->password;
+
+        $userlogged = Account::where(['emailId'=>$emailId, 'password'=>$password])->first();
+
+        if($userlogged){
+            return response()->json(['success'=>true,'data'=>$userlogged ], 200);
+        }else{
+            return response()->json(['success'=>false,'error'=>'wrong login credentials' ], 401);
+        }
+
     }
 
     /**
