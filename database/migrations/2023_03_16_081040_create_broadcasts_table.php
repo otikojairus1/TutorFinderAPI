0162ac292ea9a26ce045c0c27fd104d148264e2e
otikojairus1 <otikojairus@gmail.com>
2023-03-16 11:54:24 +0300

    chore added migration

---
+++ database/migrations/2023_03_16_081040_create_broadcasts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBroadcastsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('broadcasts', function (Blueprint $table) {
+            $table->id();
+            $table->string('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('broadcasts');
+    }
+}
